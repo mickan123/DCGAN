43e6e6de31d05b7f90828123bf6f986a9c7da579
mickan123 <mitchellmickan@gmail.com>
2018-03-17 13:02:20 +1030

    Using new architecture inspired by make.girls.moe architecutre + refactored + using wasserstein GAN loss function

--- anime_DCGAN.py
+++ anime_DCGAN.py
@@ -5,199 +5,279 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import prettytensor as pt
 import os
+import random
+from ops import *
 
-def plot(samples):
-	fig = plt.figure(figsize=(4, 4))
-	gs = gridspec.GridSpec(4, 4)
-	gs.update(wspace=0.05, hspace=0.05)
+class model(object):
 
-	for i, sample in enumerate(samples):
-		sample = (sample + 1) / 2
-		ax = plt.subplot(gs[i])
-		plt.axis('off')
-		ax.set_xticklabels([])
-		ax.set_yticklabels([])
-		ax.set_aspect('equal')
-		plt.imshow(sample.reshape(96, 96, 3), cmap='Greys_r')
+	def __init__(self):
+		self.Z_dim = 100 #Noise vector dimensions
+		self.IMAGE_DIM = 64 #output image size is IMAGE_DIM x IMAGE_DIM
+		self.mb_size = 64 #Minibatch size
+		self.print_interval = 10 #How often we print progress
+		self.n = 0 #Minibatch seed
+		self.images = self.load_images('data/faces') #Image data
+		self.disc_iterations = 15 #Number of iterations to train disc for per gen iteration
+		self.save_interval = 1000 #Save model every save_interval epochs
+		self.max_iterations = 1000000 #Max iterations to train for
 
-	return fig
+		#create output directory
+		if not os.path.exists('out/'):
+   			os.makedirs('out/')
 
-def load_images(path):
-	image_list = []
-	for image_path in glob.glob(path + "/*.png"):
-		image = misc.imread(image_path)
-		image = image.flatten()/127.5 - 1
-		image_list.append(image)
-
-	for image_path in glob.glob(path + "/*.jpg"):
-		image = misc.imread(image_path)
-		image = image.flatten()/127.5 - 1
-		image_list.append(image)
-	
-	return image_list
-
-
-def lrelu(x, th=0.2):
-    return tf.maximum(th * x, x)
-
-def minibatch_discrimination(input_layer, num_kernels, dim_per_kernel = 3):
-
-	num_features = input_layer.shape[1]
-
-	W = tf.get_variable('W', shape=[num_features, num_kernels*dim_per_kernel], initializer = tf.contrib.layers.xavier_initializer())
-	b = tf.Variable(tf.zeros([num_kernels]))
-	
-	activation = tf.matmul(input_layer, W)
-	activation = tf.reshape(activation, [-1, num_kernels, dim_per_kernel])
-
-	diffs = tf.expand_dims(activation, 3) - tf.expand_dims(tf.transpose(activation, perm=[1,2,0]), 0)
-	abs_diff = tf.reduce_sum(tf.abs(diffs), axis = 2)
-
-	minibatch_features = tf.reduce_sum(tf.exp(-abs_diff), axis = 2)
-	minibatch_features = minibatch_features + b
-
-	return tf.concat([input_layer, minibatch_features], 1)
-
-def discriminator(x, isTrain = False, reuse = False):
-	
-	with tf.variable_scope('Discriminator', reuse = tf.AUTO_REUSE):
-		conv1 = tf.layers.conv2d(x, 64, [5, 5], strides = (2,2), padding = 'same')
-		lrelu1 = lrelu(tf.layers.batch_normalization(conv1, training = isTrain))
 		
-		conv2 = tf.layers.conv2d(lrelu1, 128, [5, 5], strides = (2,2), padding = 'same')
-		lrelu2 = lrelu(tf.layers.batch_normalization(conv2, training = isTrain))
-
-		conv3 = tf.layers.conv2d(lrelu2, 256, [5, 5], strides = (2,2), padding = 'same')
-		lrelu3 = lrelu(tf.layers.batch_normalization(conv3, training = isTrain))
-
-		conv4 = tf.layers.conv2d(lrelu3, 512, [5, 5], strides = (2,2), padding = 'same')
-		lrelu4 = lrelu(tf.layers.batch_normalization(conv4, training = isTrain))
-		lrelu4_flat = tf.reshape(lrelu3, [-1, 6*6*512])
-
-		minibatch_disc = minibatch_discrimination(lrelu4_flat, 100)
-
-		fc1 = tf.layers.dense(minibatch_disc, 1, activation = lrelu)
-		D_prob = tf.nn.sigmoid(fc1)
-
-	return D_prob, fc1
-
-def generator(z, isTrain = True, reuse = False):
-
-	with tf.variable_scope('Generator', reuse = reuse):
-
-		fc0 = tf.layers.dense(z, 256*6*6, activation = lrelu)
-		lrelu0 = lrelu(tf.layers.batch_normalization(fc0, training = isTrain))
-		lrelu0 = tf.reshape(lrelu0, shape=[-1, 6, 6, 256])
-
-		conv1 = tf.layers.conv2d_transpose(lrelu0, 512, [5, 5], strides = (2,2), padding = 'same')
-		lrelu1 = lrelu(tf.layers.batch_normalization(conv1, training = isTrain))
-
-		conv2 = tf.layers.conv2d_transpose(lrelu1, 256, [5, 5], strides = (2,2), padding = 'same')
-		lrelu2 = lrelu(tf.layers.batch_normalization(conv2, training = isTrain))
-
-		conv3 = tf.layers.conv2d_transpose(lrelu2, 128, [5, 5], strides = (2,2), padding = 'same')
-		lrelu3 = lrelu(tf.layers.batch_normalization(conv3, training = isTrain))
-
-		conv4 = tf.layers.conv2d_transpose(lrelu3, 3, [5, 5], strides = (2,2), padding = 'same')
-		output = tf.nn.tanh(conv4)
-
-	return output
-
-images = load_images('/images')
+	def __call__(self):
+		self.build_model()
+		self.train_model()
 
 
-hist_averages = 5
-
-X = tf.placeholder(tf.float32, shape=[None, 27648])
-x_image = tf.reshape(X, [-1, 96, 96, 3])
-
-Z = tf.placeholder(tf.float32, shape=[None, 1, 1, 100])
-
-isTrain = tf.placeholder(dtype = tf.bool)
-
-
-G_sample = generator(Z, isTrain)
-D_real, D_logit_real = discriminator(x_image, isTrain)
-D_fake, D_logit_fake = discriminator(G_sample, isTrain, reuse = True)
-
-gen_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope = 'Generator')
-dis_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope = 'Discriminator')
-
-#Default GAN 
-"""
-D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_logit_real, labels = tf.ones_like(D_logit_real)) - 0.1)
-D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_logit_fake, labels = tf.zeros_like(D_logit_fake)) + 0.1)
-
-D_loss = D_loss_real + D_loss_fake
-G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_logit_fake, labels = tf.ones_like(D_logit_fake)))
-"""
-
-#Wasterstein GAN
-D_loss = tf.reduce_mean(D_logit_real) - tf.reduce_mean(D_logit_fake)
-G_loss = -tf.reduce_mean(D_logit_fake)
-
-clip_D = [p.assign(tf.clip_by_value(p, -0.01, 0.01)) for p in dis_vars]
-
-
-D_solver = tf.train.RMSPropOptimizer(learning_rate = 5e-4).minimize(-D_loss, var_list = dis_vars)
-G_solver = tf.train.RMSPropOptimizer(learning_rate = 5e-4).minimize(G_loss, var_list = gen_vars)
-
-if not os.path.exists('out/'):
-    os.makedirs('out/')
-
-mb_size = 64
-Z_dim = 100
-i = 0
-n = 0
-d_average = 0
-g_average = 0
-
-start_time = time.time()
-
-with tf.Session() as sess:
-
-	sess.run(tf.global_variables_initializer())
-	saver = tf.train.Saver()
-
-	#saver = tf.train.import_meta_graph('./Training Model/-10000.meta')
-	#saver.restore(sess,tf.train.latest_checkpoint('Training Model'))
-
-	for it in range(1000000):
-
-		if it % 100 == 0:
-			samples = sess.run(G_sample, feed_dict={Z: np.random.normal(0, 1, (16, 1, 1, Z_dim)), isTrain: True})
-			fig = plot(samples)
-			plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches = 'tight')
-			i += 1
-			plt.close(fig)
-
-		for _ in range(5):
-			X_mb = images[n*mb_size:(n+1)*mb_size]
-			n = (n + 1) % (len(images) // mb_size)
-			_, D_loss_curr, _ = sess.run([D_solver, D_loss, clip_D], feed_dict = {X: X_mb, Z: np.random.normal(0, 1, (mb_size, 1, 1, Z_dim)), isTrain: True})
+	#Loads images dataset normalizing and resizing them
+	def load_images(self, path):
+		image_list = []
+		for i, image_path in enumerate(glob.glob(path + "/*")):
+			image = misc.imread(image_path)
+			image = misc.imresize(image, [self.IMAGE_DIM, self.IMAGE_DIM])
+			image = image.flatten()/127.5 - 1
+			image_list.append(image)
 		
-		_, G_loss_curr = sess.run([G_solver, G_loss], feed_dict = {Z: np.random.normal(0, 1, (mb_size, 1, 1, Z_dim)), isTrain: True})
-		
-		d_average += D_loss_curr
-		g_average += G_loss_curr
+		return image_list
 
-		if it % 100 == 0:
-			d_average = d_average / 500
-			g_average = g_average / 100
-			print('Iter: {}'.format(it))
-			print('D_loss: {:.4}'. format(d_average))
-			print('G_loss: {:.4}'.format(g_average))
-			d_average = 0
-			g_average = 0
 
-			end_time = time.time()
-			print("Time taken: " + str(end_time - start_time))
-			start_time = end_time
-			print()
+	#Builds the model to be run
+	def build_model(self):
+
+		#Image input tensors
+		self.X = tf.placeholder(tf.float32, shape = [None, self.IMAGE_DIM * self.IMAGE_DIM*3])
+		self.x_image = tf.reshape(self.X, [-1, self.IMAGE_DIM, self.IMAGE_DIM, 3])
+
+		#Noise tensor
+		self.Z = tf.placeholder(tf.float32, shape = [None, self.Z_dim])
+
+		#Training tensor
+		self.isTrain = tf.placeholder(dtype = tf.bool)
+
+		self.wasserstein_GAN_loss()
+		self.build_solver()
+
+
+	#Wasserstein GAN loss function 
+	def wasserstein_GAN_loss(self):
+
+		#Get sample from generator
+		self.G_sample = self.generator(self.Z, self.isTrain)
+
+		#Get real and fake discriminator outputs
+		D_logit_real = self.discriminator(self.x_image, self.isTrain)
+		D_logit_fake = self.discriminator(self.G_sample, self.isTrain, reuse = True)
+
+		#Improved WGAN https://arxiv.org/abs/1704.00028
+		scale = 10
+		epsilon = tf.random_uniform([], 0.0, 1.0)
+		x_hat = epsilon * self.x_image + (1 - epsilon) * self.G_sample
+		d_hat = self.discriminator(x_hat, self.isTrain, reuse = True)
+		ddx = tf.gradients(d_hat, x_hat)[0]
+		ddx = tf.sqrt(tf.reduce_sum(tf.square(ddx), axis = 1))
+		ddx = tf.reduce_mean(tf.square(ddx - 1.0)) * scale
+
+		self.D_loss = tf.reduce_mean(D_logit_real) - tf.reduce_mean(D_logit_fake) + ddx
+		self.G_loss = tf.reduce_mean(D_logit_fake)
+	
+
+	#Vanilla GAN model loss function
+	def vanilla_GAN_loss():
+
+		#Get sample from generator
+		self.G_sample = self.generator(self.Z, self.isTrain)
+
+		#Get real and fake discriminator outputs
+		D_logit_real = self.discriminator(self.x_image, self.isTrain)
+		D_logit_fake = self.discriminator(G_sample, self.isTrain, reuse = True)
+
+		D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_logit_real, labels = tf.ones_like(D_logit_real)) - 0.1)
+		D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_logit_fake, labels = tf.zeros_like(D_logit_fake)) + 0.1)
+
+		self.D_loss = D_loss_real + D_loss_fake
+		self.G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = D_logit_fake, labels = tf.ones_like(D_logit_fake)))
+
+
+	#Builds the solver for graph, to be called after calling a loss function
+	def build_solver(self):
+		self.gen_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope = 'Generator')
+		self.dis_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope = 'Discriminator')
+
+		self.D_solver = tf.train.AdamOptimizer(learning_rate = 1e-4, beta1 = 0.5, beta2 = 0.9).minimize(self.D_loss, var_list = self.dis_vars)
+		self.G_solver = tf.train.AdamOptimizer(learning_rate = 1e-4, beta1 = 0.5, beta2 = 0.9).minimize(self.G_loss, var_list = self.gen_vars)
+
+
+	#Repeated block to be used in the discriminator
+	def discriminator_block(self, inputs, out, kernel_size, stride):
+	    res = inputs
+	    net = tf.layers.conv2d(inputs, out, kernel_size, stride, padding = 'same', activation = None)
+	    net = lrelu(net)
+	    net = tf.layers.conv2d(net, out, kernel_size, stride, padding = 'same', activation = None)
+	    net = net + inputs
+	    net = lrelu(net)
+	    return net
+
+
+    #Creates the discriminator network
+	def discriminator(self, x, isTrain = False, reuse = False):
+
+		with tf.variable_scope('Discriminator', reuse = tf.AUTO_REUSE):
+			net = tf.layers.conv2d(x, 32, 3, 1, padding = 'same')
+			net = lrelu(net)
+
+			net = self.discriminator_block(net, 32, 3, 1)
+
+			net = tf.layers.conv2d(net, 64, 4, 2, padding = 'same')
+			net = lrelu(net)
+
+			net = self.discriminator_block(net, 64, 3, 1)
+
+			net = tf.layers.conv2d(net, 128, 4, 2, padding = 'same')
+			net = lrelu(net)
+
+			net = self.discriminator_block(net, 128, 3, 1)
+
+			net = tf.layers.conv2d(net, 256, 3, 2, padding = 'same')
+			net = lrelu(net)
+
+			net = self.discriminator_block(net, 256, 3, 1)
+
+			net = tf.layers.conv2d(net, 512, 3, 2, padding = 'same')
+			net = lrelu(net)
+
+			net = self.discriminator_block(net, 512, 3, 1)
+
+			net = tf.layers.conv2d(net, 512, 3, 2, padding = 'same')
+			net = lrelu(net)
+
+			logits = tf.layers.dense(net, 1)
+			net = tf.nn.sigmoid(logits)
+
+		return logits
+	
+
+	#Repeated block to be used in the generator
+	def generator_block(self, inputs, out, kernel_size, stride, isTrain = True):
+
+		net = tf.layers.conv2d_transpose(inputs, out, kernel_size, stride, padding = 'same')
+		net = tf.layers.batch_normalization(net, training = isTrain)
+		net = tf.nn.relu(net)
+		net = tf.layers.conv2d_transpose(inputs, out, kernel_size, stride, padding = 'same')
+		net = tf.layers.batch_normalization(net, training = isTrain)
+		net = net + inputs
+
+		return net
+
+
+	#Creates the generator network
+	def generator(self, z, isTrain = True, reuse = False):
+
+		with tf.variable_scope('Generator', reuse = reuse):
+			net = tf.layers.dense(z, 64*16*16)
+			net = tf.layers.batch_normalization(net, training = isTrain)
+			net = tf.reshape(net, [-1, 16, 16, 64])
+			net = tf.nn.relu(net)
+
+			inputs = net
+
+			for i in range(4):
+				net = self.generator_block(net, 64, 3, 1, isTrain)
+
+			net = tf.layers.batch_normalization(net, training = isTrain)
+			net = tf.nn.relu(net)
+			net = inputs + net
+
+			net = tf.layers.conv2d_transpose(net, 256, 3, 2, padding = 'same')
+			net = tf.layers.batch_normalization(net, training = isTrain)
+			net = tf.nn.relu(net)
+
+			net = tf.layers.conv2d_transpose(net, 256, 3, 2, padding = 'same')
+			net = tf.layers.batch_normalization(net, training = isTrain)
+			net = tf.nn.relu(net)
+
+			net = tf.layers.conv2d_transpose(net, 3, 3, 1, padding = 'same')
+
+			output = tf.nn.tanh(net)
+
+		return output	
+
+
+	#Returns next batch of images
+	def next_batch(self):
+		mb = self.images[self.n*self.mb_size:(self.n+1)*self.mb_size]
+		self.n = (self.n + 1) % (len(self.images) // self.mb_size)
+		return mb
+
+
+	#Returns random noise vector
+	def noise_vec(self, size):
+ 		return np.random.normal(0, 1, (size, self.Z_dim))
+
+
+	#Trains the model
+	def train_model(self):
+		start_time = time.time()
+
+		with tf.Session() as sess:
 			
-			if (it % 10000 == 0):
-				saver.save(sess, 'Training Model/train_model', global_step = it)
+			sess.run(tf.global_variables_initializer())
 
-	saver.save(sess, 'Final Model/Final_model')
+			self.saver = tf.train.Saver()
+			
+			d_average = 0 #Average disc loss
+			g_average = 0 #Average gen loss
+
+			for it in range(self.max_iterations):
+				
+				#Train discriminator
+				for i in range(self.disc_iterations):
+					feed = {self.X: self.next_batch(), self.Z: self.noise_vec(self.mb_size), self.isTrain: True}
+					_, D_loss_curr = sess.run([self.D_solver, self.D_loss], feed_dict = feed)
+
+				#Train generator
+				feed = {self.Z: self.noise_vec(self.mb_size), self.isTrain: True}
+				_, G_loss_curr = sess.run([self.G_solver, self.G_loss], feed_dict = feed)					
+				
+				d_average += D_loss_curr
+				g_average += G_loss_curr
+				
+				#Store some progress images 
+				if it % self.print_interval == 0:
+					samples = sess.run(self.G_sample, feed_dict={self.Z: self.noise_vec(16), self.isTrain: True})
+					fig = plot(samples, self.IMAGE_DIM)
+					plt.savefig('out/{}.png'.format(str(it / self.print_interval).zfill(3)), bbox_inches = 'tight')
+					plt.close(fig)
+				
+				#Print run time statistics
+				if it % self.print_interval == 0:
+					d_average = d_average / self.print_interval
+					g_average = g_average / self.print_interval
+					print('Iter: {}'.format(it))
+					print('D_loss: {:.4}'. format(d_average))
+					print('G_loss: {:.4}'.format(g_average))
+					
+					d_average = 0
+					g_average = 0
+
+					end_time = time.time()
+					print("Time taken: " + str(end_time - start_time))
+					start_time = end_time
+					print()
+					
+				if (it % self.save_interval == 0):
+					self.saver.save(sess, 'Training Model/train_model', global_step = it)
+
+			self.saver.save(sess, 'Final Model/Final_model')
+
+
+def main():
+	GAN = model()
+	GAN()
+
+
+if __name__ == '__main__'
+	main()
+
---
+++ ops.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+#Leaky relu function
+def lrelu(x, th=0.2):
+    return tf.maximum(th * x, x)
+
+#Implements minibatch discrimination https://arxiv.org/abs/1606.03498
+def minibatch_discrimination(input_layer, num_kernels, dim_per_kernel = 3):
+
+	num_features = input_layer.shape[1]
+
+	W = tf.get_variable('W', shape=[num_features, num_kernels*dim_per_kernel], initializer = tf.contrib.layers.xavier_initializer())
+	b = tf.Variable(tf.zeros([num_kernels]))
+	
+	activation = tf.matmul(input_layer, W)
+	activation = tf.reshape(activation, [-1, num_kernels, dim_per_kernel])
+
+	diffs = tf.expand_dims(activation, 3) - tf.expand_dims(tf.transpose(activation, perm=[1,2,0]), 0)
+	abs_diff = tf.reduce_sum(tf.abs(diffs), axis = 2)
+
+	minibatch_features = tf.reduce_sum(tf.exp(-abs_diff), axis = 2)
+	minibatch_features = minibatch_features + b
+
+	return tf.concat([input_layer, minibatch_features], 1)
+
+#Returns 4x4 plot of image samples
+def plot(samples, dim):
+	fig = plt.figure(figsize=(4, 4))
+	gs = gridspec.GridSpec(4, 4)
+	gs.update(wspace=0.05, hspace=0.05)
+	for i, sample in enumerate(samples):
+		sample = (sample + 1) / 2
+		ax = plt.subplot(gs[i])
+		plt.axis('off')
+		ax.set_xticklabels([])
+		ax.set_yticklabels([])
+		ax.set_aspect('equal')
+		plt.imshow(sample.reshape(dim, dim, 3), cmap='Greys_r')
+
+	return fig
+
+
